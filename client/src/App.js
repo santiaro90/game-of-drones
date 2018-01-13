9e01e88ca6821d1449ef31ff29e12c21a6365f42
Santiago Alvarez Rodriguez <santiaro90@gmail.com>
2018-01-13 18:30:10 -0500

    Add client app placeholder

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+// @flow
+import React, { Component } from 'react'
+import { Header } from 'semantic-ui-react'
+
+class App extends Component<{}> {
+  render() {
+    return (
+      <div id="app">
+        <Header as="h1" textAlign="center">Game of Drones</Header>
+      </div>
+    )
+  }
+}
+
+export default App;
